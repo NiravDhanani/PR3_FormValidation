d9e0e25f8d6822a87eb1500c6b1323a4a9698657
ColorsKS <colorsks2021@gmail.comgit push -u origin mainremovw>
2024-04-11 20:27:44 +0530

    id-delete and inpute handler and insert

--- src/FormValidation.jsx
+++ src/FormValidation.jsx
@@ -3,27 +3,29 @@ import React, { useState } from "react";
 const FormValidation = () => {
     const [data,setData] = useState([
         {
+          id : Math.floor(Math.random()*1000),
             name : "Nirav Dhanani",
             email : "n@gmail.com",
             salary : "80000"
         },
     ])
     // second state create problem in delete function =================
-    // const [newData,setNewData] = useState([{
-    //     name : "",email : "",salary : ""
-    // }])
+    const [record,setRecord] = useState([])
 
 const NewRow = () => {
     const newRow = [...data,{
-        name : "",email : "",salary : ""
+        id:Math.floor(Math.random()*1000),name : "",email : "",salary : ""
     }
    ]
     setData(newRow)
 }
 
 
-const inputHandler = (e,i) => {
-  // const newData = e.target.value;
+const inputHandler = (e,index) => {
+  const newData = [...data];
+  newData[index][e.target.name] = e.target.value;
+  setData(newData)
+
   // setData(newData)
    console.log(e.target.value);
   };
@@ -37,6 +39,27 @@ const RemoveBtnHandler = (index) =>{
     setData(remainData)
 }
 
+const insertHandler = () =>{
+  console.log(data);
+  let r = data;
+  setRecord(r);
+  
+}
+
+// const DeleteEvent =(index)=>{
+//   let olddata = [...record];
+//   olddata.splice(index,1)
+//   setRecord(olddata)
+// }
+
+const DeleteEvent =(id)=>{
+  let olddata = [...record];
+  let del = olddata.filter((val)=>{
+    return val.id != id
+  })
+  setRecord(del)
+}
+
   return (
     <>
      <div className="row ">
@@ -48,7 +71,7 @@ const RemoveBtnHandler = (index) =>{
         <div className="container">
             <table className="table"> 
                 <thead>
-                    <th>No</th>
+                    {/* <th>No</th> */}
                     <th>Full Name</th>
                     <th>Email</th>
                     <th>Salary</th>
@@ -56,16 +79,46 @@ const RemoveBtnHandler = (index) =>{
                 </thead>
                 <tbody >
                         {data.map((item,i)=>{
-                          return  <tr key={i}>
-                                <td> <input type="text" value={i}/></td>
-                                <td> <input type="text" value={item.name} onChange={(e)=>inputHandler(e)} placeholder="Enter Full Name"  /></td>
-                                <td> <input type="text" value={item.email} onChange={(e)=>inputHandler(e)} placeholder="Enter Full Name"  /></td>
-                                <td> <input type="text" value={item.salary} onChange={(e)=>inputHandler(e)} placeholder="Enter Full Name"  /></td>
-                                <td> <button onClick={(e)=>RemoveBtnHandler(i)} className="btn btn-danger"> Delete </button> </td>
+                          return  <tr key={item.id}>
+                                 <input type="hidden" value={item.id}/>
+                                <td> <input type="text" value={item.name} name="name" onChange={(e)=>inputHandler(e,i)} placeholder="Enter Full Name"  /></td>
+                                <td> <input type="text" value={item.email} name="email" onChange={(e)=>inputHandler(e,i)} placeholder="Enter Full Name"  /></td>
+                                <td> <input type="text" value={item.salary} name="salary" onChange={(e)=>inputHandler(e,i)} placeholder="Enter Full Name"  /></td>
+                                <td> <button type="button" onClick={(e)=>RemoveBtnHandler(i)} className="btn btn-danger"> Delete </button> </td>
                             </tr>
                         })}
                 </tbody>
             </table>
+            <div className="d-flex justify-content-center">
+              <button type="button" className="btn btn-primary" onClick={(e)=>insertHandler(e)} >Insert</button>
+            </div>
+
+            <div className="mt-5">
+              <table className="table" border={1} cellPadding={10} >
+              <thead>
+                    <th>No</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Salary</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                {record.map((val,i)=>{
+                  return (
+                    <tr key={val.id}>
+                      <td> {i} </td>
+                      <td> {val.name} </td>
+                      <td> {val.email} </td>
+                      <td> {val.salary} </td>
+                      <td> 
+                        <button className="btn btn-danger" onClick={(e)=>DeleteEvent(val.id)} >Delete</button>
+                         </td>
+                    </tr>
+                  )
+                })}
+                </tbody>
+              </table>
+            </div>
         </div>
       </div>
     
